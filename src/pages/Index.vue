346256ff9aed470bb70640c38302de3b61f89449
amodinho <u14284783@tuks.co.za>
2019-07-19 12:00:57 +0200

    tailwind config is operational

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,7 +2,7 @@
   <Layout>
     
     <h1 class="text-green-400">Newsroom</h1>
-    <button class="py-10 px-10 text-green-dark blue-dark hover:blue-dark"> hi</button>
+    <button class="py-10 px-10 text-pinkDarkest blue-dark hover:blue-dark"> hi</button>
   </Layout>
 </template>
 
