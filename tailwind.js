21ca453a124ab2b5167e10774aa61c98be81a3e9
amodinho <u14284783@tuks.co.za>
2019-07-22 16:43:04 +0200

    added border widths

--- tailwind.js
+++ tailwind.js
@@ -87,6 +87,15 @@ module.exports ={
                 'cover': 'cover',
                 'contain': 'contain',
               },
+              borderWidths: {
+                default: '1px',
+                '0': '0',
+                '2': '2px',
+                '4': '4px',
+                '8': '8px',
+              },
+
+              
         }
     }
 }
