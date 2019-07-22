5140f8be4f1de98e18c4dc920e662e630f6edcf0
amodinho <u14284783@tuks.co.za>
2019-07-22 16:53:34 +0200

    added navbar

--- src/components/navigation/NavBar.vue
+++ src/components/navigation/NavBar.vue
@@ -1,5 +1,42 @@
 <template>
-    
+     <nav class="border-top flex items-center justify-between flex-wrap bg-green-darker p-6">
+        <div class="flex items-center flex-no-shrink text">
+            <h1 class="font-display text-white">
+            <a 
+            class="no-underline text-white"
+            href="/">
+            Pimp My Book
+            </a>
+            </h1>
+        </div>
+        <div class="block lg:hidden">
+            <span @click="toggle" class="flex items-center px-3 py-2">
+                <img src="../../assets/menu.svg"/>
+                </span>
+        </div>
+        <div :class="open ? 'block sm-600:block md:block sm-land:block':'hidden'" class="w-full flex-grow sm:hidden lg:flex lg:items-center lg:w-auto">
+            <div class=" text-md sm:flex-grow ">
+                
+                 <a href="#" class="block mt-4 no-underline sm:inline-block sm:mt-0 text-white hover:text-bg-green-light mr-4">
+                   
+                       <router-link class="text-white no-underline" to="/how-it-works">
+                       <p class="font-body hover:text-green">
+                        How It Works
+                        </p>
+                        </router-link>
+                 </a>
+              
+            </div>
+            <div class="nav__drawer--right">
+                <p class="text-white font-body mr-4 hover:text-green">
+                  
+                </p>
+            </div>
+            <div class="nav__drawer--right ">
+             
+            </div>
+        </div>
+    </nav>
 </template>
 <script>
 export default {
