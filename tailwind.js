da31978a1a651816ed5b5cd55a8576270d460136
amodinho <u14284783@tuks.co.za>
2019-07-22 16:47:19 +0200

    added shadows

--- tailwind.js
+++ tailwind.js
@@ -211,6 +211,15 @@ module.exports ={
                 '24': '6rem',
                 '32': '8rem',
               },
+              shadows: {
+                default: '0px 4px 4px  rgba(0,0,0,0.25)',
+                'md': '0 4px 8px 0 rgba(0,0,0,0.12), 0 2px 4px 0 rgba(0,0,0,0.08)',
+                'lg': '0 15px 30px 0 rgba(0,0,0,0.11), 0 5px 15px 0 rgba(0,0,0,0.08)',
+                'inner': 'inset 0 2px 4px 0 rgba(0,0,0,0.06)',
+                'outline': '0 0 0 3px rgba(52,144,220,0.5)',
+                'lg-card': '15px -10px 4px rgba(0,0,0,0.25)',
+                'none': 'none',
+              },
         }
     }
 }
