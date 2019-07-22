988ba1772a542adaf1f47a0a0e83a64f4128b046
amodinho <u14284783@tuks.co.za>
2019-07-22 16:41:09 +0200

    added font weights

--- tailwind.js
+++ tailwind.js
@@ -63,6 +63,17 @@ module.exports ={
                 '5xl': '3rem', 
                 '7xl': '5rem'     // 48px
               },
+              fontWeights: {
+                'hairline': 100,
+                'thin': 200,
+                'light': 300,
+                'normal': 400,
+                'medium': 500,
+                'semibold': 600,
+                'bold': 700,
+                'extrabold': 800,
+                'black': 900,
+              },
         }
     }
 }
