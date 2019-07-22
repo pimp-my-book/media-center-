158c738f1eb40332d0e65789ca388956ae74550e
amodinho <u14284783@tuks.co.za>
2019-07-22 17:57:12 +0200

    it's fixed

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,8 +1,7 @@
 <template>
   <Layout>
     
-    <p class="text-green-400 font-body text-4xl">Newsroom</p>
-    <button class="py-10 px-10  text-pinkDarkest blue-dark hover:blue-dark"> hi</button>
+   
   </Layout>
 </template>
 
