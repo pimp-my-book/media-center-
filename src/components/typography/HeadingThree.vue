1438c44aeb85dacd3f90afcc89055bdda1488f80
amodinho <u14284783@tuks.co.za>
2019-07-23 11:44:49 +0200

    done with heading Three

---
+++ src/components/typography/HeadingThree.vue
@@ -0,0 +1,11 @@
+<template>
+    <p class="text-3xl font-display">{{text}}</p>
+</template>
+<script>
+export default {
+    name:'HeadingThree',
+    props: {
+        text: String
+    }
+}
+</script>
