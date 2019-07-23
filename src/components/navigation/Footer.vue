f7be32f17e16ac508f55b6983160fc4e7907f3d6
amodinho <u14284783@tuks.co.za>
2019-07-23 11:08:46 +0200

    footer is responsvie for mobile

--- src/components/navigation/Footer.vue
+++ src/components/navigation/Footer.vue
@@ -6,21 +6,21 @@
            <p>Our Products</p>
             <p>Our Products</p>
           </div>
-          <div class="flex flex-col x:ml-32">
+          <div class="flex flex-col x:ml-32 s:mt-5">
           <p>Make Money With Us</p>
            <p>Our Products</p>
             <p>Our Products</p>
           </div>
 
-          <div class="flex flex-col  x:ml-32">
+          <div class="flex flex-col  x:ml-32 s:mt-5">
               <PmbLogo/>
               <div class="flex flex-row">
                   <TwitterLogo/>
                   <FbLogo/>
               </div>
           </div>
-          <div class="flex flex-col  x:ml-32">Terms</div>
-          <div class="flex flex-row  x:ml-10">
+          <div class="flex flex-col  x:ml-32 s:mt-5">Terms</div>
+          <div class="flex flex-row  x:ml-10 s:mt-5" >
               <div>&copy; {{new Date().getFullYear()}}, 
               <img src="https://s3.amazonaws.com/goa-assets/Icons/flag-for-south-africa_1f1ff-1f1e6.svg"/></div>
               </div>
