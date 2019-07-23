777491401c1c851d213a19ede19d9087617c5c3d
amodinho <u14284783@tuks.co.za>
2019-07-23 11:44:27 +0200

    done with headingtwo

---
+++ src/components/typography/HeadingTwo.vue
@@ -0,0 +1,11 @@
+<template>
+    <p class="text-4xl font-display">{{text}}</p>
+</template>
+<script>
+export default {
+    name:'HeadingTwo',
+    props: {
+        text: String
+    }
+}
+</script>
