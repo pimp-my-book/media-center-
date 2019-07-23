56e7138247f18cdf88bf950be108ab79198c54b9
amodinho <u14284783@tuks.co.za>
2019-07-23 11:50:00 +0200

    added stucture

---
+++ src/components/cards/ArticleCard.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <!-- article img -->
+        <img/>
+        <div>
+             <!-- article tag -->
+ 
+            <!-- article title -->
+
+
+            <!-- article date -->
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name:'ArticleCard'
+}
+</script>
