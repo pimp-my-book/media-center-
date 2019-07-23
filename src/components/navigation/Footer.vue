a1cf69b5a72238e4a358a79011e348d8ef2c6096
amodinho <u14284783@tuks.co.za>
2019-07-23 11:21:26 +0200

    fixed footer for ipads

--- src/components/navigation/Footer.vue
+++ src/components/navigation/Footer.vue
@@ -7,21 +7,21 @@
            <p>Our Products</p>
             <p>Our Products</p>
           </div>
-          <div class="flex flex-col x:ml-32 s:mt-5">
+          <div class="flex flex-col x:ml-32 md:ml-16 s:mt-5 md:mt-0">
           <p>Make Money With Us</p>
            <p>Our Products</p>
             <p>Our Products</p>
           </div>
 
-          <div class="flex flex-col  x:ml-32 s:mt-5">
+          <div class="flex flex-col  x:ml-32 md:ml-16 s:mt-5 md:mt-0">
               <PmbLogo/>
               <div class="flex flex-row">
                   <TwitterLogo/>
                   <FbLogo/>
               </div>
           </div>
-          <div class="flex flex-col  x:ml-32 s:mt-5">Terms</div>
-          <div class="flex flex-row  x:ml-10 s:mt-5" >
+          <div class="flex flex-col  x:ml-32 md:ml-16 s:mt-5 md:mt-0">Terms</div>
+          <div class="flex flex-row  x:ml-10 md:ml-16 s:mt-5 md:mt-0" >
               <div>&copy; {{new Date().getFullYear()}}, 
               <img src="https://s3.amazonaws.com/goa-assets/Icons/flag-for-south-africa_1f1ff-1f1e6.svg"/></div>
               </div>
