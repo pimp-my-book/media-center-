1c51f3ff087db347f1df5fa6b2cf27cd0e7700f7
amodinho <u14284783@tuks.co.za>
2019-07-24 14:43:23 +0200

    added some padding

--- src/components/AllPosts.vue
+++ src/components/AllPosts.vue
@@ -4,12 +4,13 @@
 
    <div class="mt-20">
 <HeadingFour
+class="p-5"
 text="All Articles"
 />
 
 <!-- start of All articles Grid -->
 
-<div class="All__Grid">
+<div class="All__Grid p-5">
  <div v-for="allEdges in $static.allContent.edges" :key="allEdges.node.id">
 <!-- lastest articles -->
  <ArticleCard
@@ -41,6 +42,7 @@ export default {
 .All__Grid{
      display:grid;
  grid-gap: 0px;
+ align-items: end;
   grid-template-columns: repeat(3, minmax(320px, 1fr));
 }
 </style>
