3987b1b9885a293d9bb1c8b78d39eb0aee37ec23
amodinho <u14284783@tuks.co.za>
2019-07-24 11:45:40 +0200

    done with card

--- src/components/cards/HomeCard.vue
+++ src/components/cards/HomeCard.vue
@@ -1,12 +1,24 @@
 <template>
     <div class="card w-300 h-300">
-      
+      <HeadingOne
+      text="Welcome to Newsroom"
+      />
+      <HeadingFour
+      text="This is Pimp My Book's all access area where you can get all annoucments, updates and blog posts fresh off the press."
+       />
     </div>
 </template>
 
 
 <script>
+import HeadingOne from "../typography/HeadingOne"
+import HeadingFour from "../typography/HeadingFour"
+
 export default {
-    name: 'HomeCard'
+    name: 'HomeCard',
+    components:{
+        HeadingFour,
+        HeadingOne
+    }
 }
 </script>
