4b1286772cb5ca267881b6b66c957f84882a610d
amodinho <u14284783@tuks.co.za>
2019-07-24 12:13:26 +0200

    Added grid

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -13,7 +13,8 @@
      text="Latest"
      />
 
-     <ArticleCard
+<div class="flex flex-row mr-5">
+  <ArticleCard
     img="https://images.unsplash.com/photo-1562819474-6ccdd832fe2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
     cardTag="Blog"
     cardTitle="How To Run PMB"
@@ -26,6 +27,8 @@
     cardTitle="How To Run PMB"
     cardDate="2019-09-08"
     /> 
+</div>
+   
     
 
    </div>
