f70d28882275d141c4926639e310436a197096f4
amodinho <u14284783@tuks.co.za>
2019-07-24 12:02:57 +0200

    Testing layout

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -9,6 +9,24 @@
     />
    </div>
    <div>
+     <HeadingFour
+     text="Latest"
+     />
+
+     <ArticleCard
+    img="https://images.unsplash.com/photo-1562819474-6ccdd832fe2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+    cardTag="Blog"
+    cardTitle="How To Run PMB"
+    cardDate="2019-09-08"
+    /> 
+
+    <ArticleCard
+    img="https://images.unsplash.com/photo-1562819474-6ccdd832fe2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+    cardTag="Blog"
+    cardTitle="How To Run PMB"
+    cardDate="2019-09-08"
+    /> 
+    
 
    </div>
     
@@ -20,10 +38,13 @@
 <script>
 import HomeCard from "../components/cards/HomeCard"
 import ArticleCard from "../components/cards/ArticleCard"
+import HeadingFour from "../components/typography/HeadingFour"
+
 export default {
   components:{
     ArticleCard,
-    HomeCard
+    HomeCard,
+    HeadingFour
   },
   metaInfo: {
     title: 'PMB Newsroom'
