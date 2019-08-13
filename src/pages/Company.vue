8cc80b736a0f85e4a9157a03309b38d63df90dbf
amodinho <u14284783@tuks.co.za>
2019-08-13 09:54:56 +0200

    added the mission

--- src/pages/Company.vue
+++ src/pages/Company.vue
@@ -8,18 +8,31 @@
     text="Pimp My Book started out in 2006 with the goal of making textbooks affordable. We managed to achieve this pretty well by expanding to 5 stores across South Africa. However, no great company can sit still which is why we spawned a bursary division in 2017. Which helps bursaries save several millions on academic material."
     />
 </div>
+
+<div>
+    <div>
+        <HeadingFour
+    text="Our Mission"
+    />
+    <Body
+    text="We make educational materials and supporting technologies affordable and accessible to students."
+    />
+    </div>
+</div>
 </Layout>
 </template>
 
 <script>
 import HeadingOne from '../components/typography/HeadingOne'
 import HeadingFour from '../components/typography/HeadingFour'
+import Body from '../components/typography/Body'
 
 export default {
     name: "Company",
     components:{
         HeadingOne,
-        HeadingFour
+        HeadingFour,
+        Body
     }
 }
 </script>
