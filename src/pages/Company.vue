682eb8aaba400c0f6879dc5322583126714d264d
amodinho <u14284783@tuks.co.za>
2019-08-16 11:44:11 +0200

    Added airtable embed

--- src/pages/Company.vue
+++ src/pages/Company.vue
@@ -61,6 +61,15 @@
     </div>
 </div>
 
+<div>
+     <HeadingThree
+     class="text-pinkDarkest"
+     text="Our Team"
+     />
+    <iframe class="airtable-embed" src="https://airtable.com/embed/shrBBIrIZ6arTUhSm?backgroundColor=pink&viewControls=on" frameborder="0" onmousewheel="" width="100%" height="533" style="background: transparent; border: 1px solid #ccc;"/>
+
+</div>
+
 </Layout>
 </template>
 
